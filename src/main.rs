d10fa97cb04b63140b7d98cd9fa11990397d34b6
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-08 16:47:15 -0300

    feat: create virtual device

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
 }
 
 impl App {
@@ -86,12 +87,14 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = Self::create_instance(window, &entry, &mut data)?;
-        pick_physical_device(&instance, &mut data)?;
+        Self::pick_physical_device(&instance, &mut data)?;
 
+        let device = Self::create_logical_device(&entry, &instance, &mut data)?;
         Ok(Self {
             entry,
             instance,
             data,
+            device,
         })
     }
 
@@ -167,6 +170,73 @@ impl App {
         Ok(instance)
     }
 
+    unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
+        for physical_device in instance.enumerate_physical_devices()? {
+            let properties = instance.get_physical_device_properties(physical_device);
+
+            if let Err(error) = Self::check_physical_device(instance, data, physical_device) {
+                warn!(
+                    "Skipping physical device (`{}`): {}",
+                    properties.device_name, error
+                );
+            } else {
+                info!("Selected physical device (`{}`).", properties.device_name);
+                data.physical_device = physical_device;
+                return Ok(());
+            }
+        }
+
+        Err(anyhow!("Failed to find suitable physical device."))
+    }
+
+    unsafe fn check_physical_device(
+        instance: &Instance,
+        data: &AppData,
+        physical_device: vk::PhysicalDevice,
+    ) -> Result<()> {
+        QueueFamilyIndices::get(instance, data, physical_device)?;
+        Ok(())
+    }
+
+    unsafe fn create_logical_device(
+        entry: &Entry,
+        instance: &Instance,
+        data: &mut AppData,
+    ) -> Result<Device> {
+        let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+        let queue_priorities = &[1.0];
+        let queue_info = vk::DeviceQueueCreateInfo::builder()
+            .queue_family_index(indices.graphics)
+            .queue_priorities(queue_priorities);
+
+        let layers = if Self::VALIDATION_ENABLED {
+            vec![Self::VALIDATION_LAYER.as_ptr()]
+        } else {
+            vec![]
+        };
+
+        let mut extensions = vec![];
+
+        // Required by Vulkan SDK on macOS since 1.3.216.
+        if cfg!(target_os = "macos") && entry.version()? >= Self::PORTABILITY_MACOS_VERSION {
+            extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+        }
+
+        let features = vk::PhysicalDeviceFeatures::builder();
+        let queue_infos = &[queue_info];
+        let info = vk::DeviceCreateInfo::builder()
+            .queue_create_infos(queue_infos)
+            .enabled_layer_names(&layers)
+            .enabled_extension_names(&extensions)
+            .enabled_features(&features);
+        let device = instance.create_device(data.physical_device, &info, None)?;
+        
+        data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+        Ok(device)
+    }
+
     /// Renders a frame for our Vulkan app.
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
         Ok(())
@@ -174,6 +244,8 @@ impl App {
 
     /// Destroys our Vulkan app.
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
+
         if Self::VALIDATION_ENABLED {
             self.instance
                 .destroy_debug_utils_messenger_ext(self.data.messenger, None);
@@ -188,39 +260,12 @@ impl App {
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
 
 #[derive(Debug, Error)]
 #[error("Missing {0}.")]
 pub struct SuitabilityError(pub &'static str);
-
-unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
-    for physical_device in instance.enumerate_physical_devices()? {
-        let properties = instance.get_physical_device_properties(physical_device);
-
-        if let Err(error) = check_physical_device(instance, data, physical_device) {
-            warn!(
-                "Skipping physical device (`{}`): {}",
-                properties.device_name, error
-            );
-        } else {
-            info!("Selected physical device (`{}`).", properties.device_name);
-            data.physical_device = physical_device;
-            return Ok(());
-        }
-    }
-
-    Err(anyhow!("Failed to find suitable physical device."))
-}
-
-unsafe fn check_physical_device(
-    instance: &Instance,
-    data: &AppData,
-    physical_device: vk::PhysicalDevice,
-) -> Result<()> {
-    QueueFamilyIndices::get(instance, data, physical_device)?;
-    Ok(())
-}
 
 #[derive(Copy, Clone, Debug)]
 struct QueueFamilyIndices {
