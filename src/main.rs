6296c18035a4f2936d49350ce8bf170dc5bd6fd9
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-09 17:07:42 -0300

    feat: add present queue

--- src/main.rs
+++ src/main.rs
@@ -226,16 +226,32 @@ impl App {
             extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
         }
 
+        let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+        let mut unique_indices = HashSet::new();
+        unique_indices.insert(indices.graphics);
+        unique_indices.insert(indices.present);
+
+        let queue_priorities = &[1.0];
+        let queue_infos = unique_indices
+            .iter()
+            .map(|i| {
+                vk::DeviceQueueCreateInfo::builder()
+                    .queue_family_index(*i)
+                    .queue_priorities(queue_priorities)
+            })
+            .collect::<Vec<_>>();
+
         let features = vk::PhysicalDeviceFeatures::builder();
-        let queue_infos = &[queue_info];
         let info = vk::DeviceCreateInfo::builder()
-            .queue_create_infos(queue_infos)
+            .queue_create_infos(&queue_infos)
             .enabled_layer_names(&layers)
             .enabled_extension_names(&extensions)
             .enabled_features(&features);
         let device = instance.create_device(data.physical_device, &info, None)?;
-        
+
         data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+        data.present_queue = device.get_device_queue(indices.present, 0);
 
         Ok(device)
     }
@@ -266,6 +282,7 @@ struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
     graphics_queue: vk::Queue,
+    present_queue: vk::Queue,
 }
 
 #[derive(Debug, Error)]
@@ -275,6 +292,7 @@ pub struct SuitabilityError(pub &'static str);
 #[derive(Copy, Clone, Debug)]
 struct QueueFamilyIndices {
     graphics: u32,
+    present: u32,
 }
 
 impl QueueFamilyIndices {
@@ -285,13 +303,25 @@ impl QueueFamilyIndices {
     ) -> Result<Self> {
         let properties = instance.get_physical_device_queue_family_properties(physical_device);
 
+        let mut present = None;
+        for (index, properties) in properties.iter().enumerate() {
+            if instance.get_physical_device_surface_support_khr(
+                physical_device,
+                index as u32,
+                data.surface,
+            )? {
+                present = Some(index as u32);
+                break;
+            }
+        }
+
         let graphics = properties
             .iter()
             .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
             .map(|i| i as u32);
 
-        if let Some(graphics) = graphics {
-            Ok(Self { graphics })
+        if let (Some(graphics), Some(present)) = (graphics, present) {
+            Ok(Self { graphics, present })
         } else {
             Err(anyhow!(SuitabilityError(
                 "Missing required queue families."
