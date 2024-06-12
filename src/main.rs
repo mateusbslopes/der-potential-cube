6818d2469d0f0b2cb7612b01b69bf63b13f0590e
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-11 21:06:04 -0300

    feat: create swapchain image views

--- src/main.rs
+++ src/main.rs
@@ -98,6 +98,7 @@ impl App {
 
         let device = Self::create_logical_device(&entry, &instance, &mut data)?;
         Self::create_swapchain(window, &instance, &device, &mut data)?;
+        Self::create_swapchain_image_views(&device, &mut data)?;
         Ok(Self {
             entry,
             instance,
@@ -378,6 +379,38 @@ impl App {
         Ok(())
     }
 
+    unsafe fn create_swapchain_image_views(device: &Device, data: &mut AppData) -> Result<()> {
+        data.swapchain_image_views = data
+            .swapchain_images
+            .iter()
+            .map(|i| {
+                let components = vk::ComponentMapping::builder()
+                    .r(vk::ComponentSwizzle::IDENTITY)
+                    .g(vk::ComponentSwizzle::IDENTITY)
+                    .b(vk::ComponentSwizzle::IDENTITY)
+                    .a(vk::ComponentSwizzle::IDENTITY);
+
+                let subresource_range = vk::ImageSubresourceRange::builder()
+                    .aspect_mask(vk::ImageAspectFlags::COLOR)
+                    .base_mip_level(0)
+                    .level_count(1)
+                    .base_array_layer(0)
+                    .layer_count(1);
+
+                let info = vk::ImageViewCreateInfo::builder()
+                    .image(*i)
+                    .view_type(vk::ImageViewType::_2D)
+                    .format(data.swapchain_format)
+                    .components(components)
+                    .subresource_range(subresource_range);
+
+                device.create_image_view(&info, None)
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(())
+    }
+
     /// Renders a frame for our Vulkan app.
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
         Ok(())
@@ -385,6 +418,11 @@ impl App {
 
     /// Destroys our Vulkan app.
     unsafe fn destroy(&mut self) {
+        self.data
+            .swapchain_image_views
+            .iter()
+            .for_each(|v| self.device.destroy_image_view(*v, None));
+
         self.device.destroy_swapchain_khr(self.data.swapchain, None);
         self.device.destroy_device(None);
 
@@ -410,6 +448,7 @@ struct AppData {
     swapchain_images: Vec<vk::Image>,
     swapchain_format: vk::Format,
     swapchain_extent: vk::Extent2D,
+    swapchain_image_views: Vec<vk::ImageView>,
 }
 
 #[derive(Debug, Error)]
